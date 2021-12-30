5638e862550a0fa6a9549a90fc06a5076736a8eb
Ed-CodePower <94185457+Ed-CodePower@users.noreply.github.com>
2021-12-30 16:05:41 -0500

    Delete Project_1 directory

--- Project_1/Project_1.Api/Models/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Project_1.Api.Models
-{
-    public class Customer
-    {
-        public int CustomerId { get; set; }
-        public string? FirstName { get; set; }
-        public bool IsManager { get; set; }
-    }
-}
